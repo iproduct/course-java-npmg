7e90f856071da015d2f74c4bbfdb29620b18f4ec
Trayan Iliev <office@iproduct.org>
2017-10-04 12:20:54 +0300

    constructor and toString added

--- store/src/store/Person.java
+++ store/src/store/Person.java
@@ -5,13 +5,32 @@ public class Person {
 	String name; // default null
 	int age;
 	String address;
-	
+
+	public Person(long ssn, String name, int age, String address) {
+		this.ssn = ssn;
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [ssn=" + ssn + ", name=" + name + ", age=" + age + ", "
+				+ "address=" + address + "]";
+	}
+
 	public static void main(String[] args) {
-		Person p1 = new Person();
-		p1.ssn = 0423111234;
-		p1.name = "Petar Dimitrov";
-		System.out.println("SSN: " + p1.ssn);
-		System.out.println("Name: " + p1.name);
+		Person p1 = new Person(423111234, "Petar Dimitrov", 17, "Sofia 1000");
+		Person p2 = new Person(1234567890, "Ivan Petrov", 17, "Sofia 1000");
+
+		// System.out.printf("SSN: %010d\n", p1.ssn);
+		// System.out.println("Name: " + p1.name);
+		//
+		// System.out.printf("SSN: %010d\n", p2.ssn);
+		// System.out.println("Name: " + p2.name);
+
+		System.out.println(p1);
+		System.out.println(p2);
 	}
 
 }
