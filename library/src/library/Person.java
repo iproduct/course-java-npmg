f846fa4eede3f822412ae8b4ff4fedfd5741e328
Trayan Iliev <office@iproduct.org>
2017-10-11 08:13:28 +0300

    constructor and toString added

--- library/src/library/Person.java
+++ library/src/library/Person.java
@@ -7,21 +7,37 @@ public class Person {
 	String name; //null by default
 	Date birthDate;
 	String address;
+	
+	public Person() {
+	}
+	
+	public Person(long anSsn, String name, Date birthDate, String address) {
+		ssn = anSsn;
+		this.name = name;
+		this.birthDate = birthDate;
+		this.address = address;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [ssn=" 
+				+ String.format("SSN: %010d\n", ssn)
+				+ ", name=" + name + ", birthDate=" + birthDate 
+				+ ", address=" + address + "]";
+	}
 
 	public static void main(String[] args) {
-		Person p1 = new Person();
-		p1.ssn = 1234567890;
-		p1.name = "Ivan Petrov";
-		
-		Person p2 = new Person();
-		p2.ssn = 42303466;
-		p2.name = "Dimitar Atanasov";
+		Person p1 = new Person(1234567890, "Ivan Petrov", new Date(), "Sofia 1000");
+		Person p2 = new Person(42303466, "Dimitar Atanasov", new Date(), "Provdiv");
 
-		System.out.printf("SSN: %010d\n", p1.ssn);
-		System.out.println("Name: " + p1.name);
+		System.out.println(p1);
+		System.out.println(p2);
 		
-		System.out.printf("SSN: %010d\n", p2.ssn);
-		System.out.println("Name: " + p2.name);
+//		System.out.printf("SSN: %010d\n", p1.ssn);
+//		System.out.println("Name: " + p1.name);
+//		
+//		System.out.printf("SSN: %010d\n", p2.ssn);
+//		System.out.println("Name: " + p2.name);
 
 
 	}
