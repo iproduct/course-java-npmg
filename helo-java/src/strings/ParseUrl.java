2599ea54bdf8f24ac8cc160bb0559a14f6d83e2b
Trayan Iliev <ilievt@gmail.com>
2017-10-25 11:47:05 +0300

    ParseUrl demo initial commit.

--- helo-java/src/strings/ParseUrl.java
+++ helo-java/src/strings/ParseUrl.java
@@ -1,6 +1,7 @@
 package strings;
 
 public class ParseUrl {
+	public static final String URL = "https://github.com/iproduct/course-java-npmg.git"; 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
