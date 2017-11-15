6a1f9f8e0d6148c5e29ddf5de2b38eacebbd16ac
Trayan Iliev <ilievt@gmail.com>
2017-11-15 12:51:33 +0200

    sorting added

--- helo-java/src/arrays_11i/WordsIndexing.java
+++ helo-java/src/arrays_11i/WordsIndexing.java
@@ -1,19 +1,32 @@
 package arrays_11i;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
-class WordCountTupple {
+class WordCountTupple  implements Comparable<WordCountTupple> {
 	String word;
 	int count;
+	
+	@Override
+	public String toString() {
+		return String.format("WordCountTupple [word=%s, count=%s]", word, count);
+	}
+
+	@Override
+	public int compareTo(WordCountTupple o) {
+		return this.count < o.count ? 1 : this.count == o.count ? 0 : -1;
+	}
+
+	
 }
 
 public class WordsIndexing {
 
 	private static int findIndex(
-			String[] array, int count, String element) {
+			WordCountTupple[] array, int count, String element) {
 		for(int i = 0;  i < count; i++) {
-			if(array[i].equals(element)) {
+			if(array[i].word.equals(element)) {
 				return i;
 			}
 		}
@@ -32,24 +45,31 @@ public class WordsIndexing {
 		System.out.println(Arrays.toString(words));
 		
 		// 3. Compute unique words counts
-		String[] uniqueWords = new String[words.length];
-		int[] uniqueWordCounts = new int[words.length];
+		WordCountTupple[] tupples = new WordCountTupple[words.length];
 		int counter = 0;
 		for(String word: words) {
-			int index = findIndex(uniqueWords, counter, word);
+			int index = findIndex(tupples, counter, word);
 			if (index < 0) { // new word
-				uniqueWords[counter] = word;
-				uniqueWordCounts[counter] = 1;
+				tupples[counter] = new WordCountTupple();
+				tupples[counter].word = word;
+				tupples[counter].count = 1;
 				counter++;
 			} else { // repeated word
-				uniqueWordCounts[index] ++;
+				tupples[index].count ++;
 			}
 		}
 		
 		// 4. Print words & counts
 		for(int i =0; i < counter; i++) {
 			System.out.println(	String.format(
-					"%s -> %d", uniqueWords[i], uniqueWordCounts[i]));
+					"%s -> %d", tupples[i].word, tupples[i].count));
+		}
+		
+		// 5. Sort by count
+		WordCountTupple[] results = Arrays.copyOf(tupples, counter);
+		Arrays.sort(results);
+		for(int i =0; i < counter; i++) {
+			System.out.println(	results[i]);
 		}
 	}
 
