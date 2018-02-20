076bf4a28a2745266519f1465d69021cf3b1bde1
Trayan Iliev <ilievt@gmail.com>
2018-02-20 17:24:14 +0200

    problems solutions added

---
+++ zadachi/src/zarcheta/Tabla.java
@@ -0,0 +1,13 @@
+package zarcheta;
+
+import java.util.Random;
+
+public class Tabla {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int d1 = rand.nextInt(6) + 1;
+
+	}
+
+}
