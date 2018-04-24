71e1296cb10e404b5d870243ca55d83e1eec8dd1
Trayan Iliev <ilievt@gmail.com>
2018-04-24 15:12:00 +0300

    results text area added

--- library-gui/src/library/view/MainFrame.java
+++ library-gui/src/library/view/MainFrame.java
@@ -1,4 +1,5 @@
 package library.view;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
@@ -16,6 +17,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.CompoundBorder;
@@ -23,8 +25,6 @@ import javax.swing.border.LineBorder;
 import javax.swing.border.MatteBorder;
 
 import library.model.Book;
-
-
 
 public class MainFrame extends JFrame {
 	private JLabel label;
@@ -37,7 +37,10 @@ public class MainFrame extends JFrame {
 	private JTextField jtfAuthors = new JTextField(20);
 	private JTextField jtfPublisher = new JTextField(20);
 	private JTextField jtfYear = new JTextField(20);
-	
+	private JPanel resultsPanel = new JPanel();
+	private JTextArea jtaResults = new JTextArea(15, 60);
+	private Box centralPanel = Box.createVerticalBox();
+
 	private class AddBookListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -47,6 +50,7 @@ public class MainFrame extends JFrame {
 			book.setPublisher(jtfPublisher.getText());
 			try {
 				book.setYear(Integer.parseInt(jtfYear.getText()));
+				jtfYear.setBorder(new LineBorder(Color.GRAY));
 			} catch (NumberFormatException ex) {
 				jtfYear.setBorder(new LineBorder(Color.RED));
 			}
@@ -56,43 +60,48 @@ public class MainFrame extends JFrame {
 
 	public MainFrame() throws HeadlessException {
 		super("Library Manager");
-		
+
 		add(BorderLayout.NORTH, new JButton("Main Menu"));
-//		add(BorderLayout.WEST, new JButton("West"));
-//		add(BorderLayout.EAST, new JButton("East"));
-		
-		//Build buttonPanel
+		// add(BorderLayout.WEST, new JButton("West"));
+		// add(BorderLayout.EAST, new JButton("East"));
+
+		// Build buttonPanel
 		buttonPanel.setLayout(new FlowLayout());
 		buttonPanel.add(btnSubmit);
 		buttonPanel.add(btnReset);
 		buttonPanel.add(btnCancel);
-		add(BorderLayout.SOUTH, buttonPanel);
-		
-		//Build mainPanel
-		mainPanel.setLayout(new GridLayout(4,2));
+		add(BorderLayout.SOUTH, buttonPanel);		
+
+		// Build mainPanel
+		mainPanel.setLayout(new GridLayout(4, 2));
 		mainPanel.setBorder(new MatteBorder(20, 50, 20, 50, new Color(0, 0, 0, 0)));
 		makeField("Title:", jtfTitle, mainPanel);
 		makeField("Authors:", jtfAuthors, mainPanel);
 		makeField("Publisher:", jtfPublisher, mainPanel);
 		makeField("Year:", jtfYear, mainPanel);
-		add(mainPanel);
-		
+
 		btnSubmit.addActionListener(new AddBookListener());
 		
+		//Results panel
+		resultsPanel.add(jtaResults);
+		centralPanel.add(mainPanel);
+		centralPanel.add(resultsPanel);
+		add(centralPanel);
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 600);
-//		setBounds(new Rectangle(300, 200, 600, 400));
+		// setBounds(new Rectangle(300, 200, 600, 400));
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
-	
+
 	class ChangeLabelListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			label.setText("Books data here ...");	
-		}	
+			label.setText("Books data here ...");
+		}
 	}
-	
+
 	private void makeField(String fieldName, Component component, Container container) {
 		Box panel = Box.createVerticalBox();
 		panel.add(Box.createVerticalGlue());
